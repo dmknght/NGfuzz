851d41ff33e7fbd67965b790f0bd46d8cbbc2e71
dmknght <dmknght@localhost.localdomain>
2019-07-11 09:11:38 +0700

    Spider with multiple parameters

--- cores/spider.py
+++ cores/spider.py
@@ -5,45 +5,14 @@ def get_params(url):
 	param, value = "", ""
 	try:
 		url, payloads = url.split("?")
-		param, value = [_.split("=") for _ in payloads.split("&")][0]
-
+		params = {param: value for param, value in [_.split("=") for _ in payloads.split("&")]}
 	except ValueError:
 		pass
 	except Exception as error:
 		from cores import events
 		events.error(error)
 	finally:
-		return {url: {param: value}}
-
-# recursion
-# def spider(url):
-# 	from modules import check_robots
-# 	# global all_urls
-# 	all_urls = []
-
-# 	all_urls = check_robots.check(url)
-
-# 	# scope = get_domain(url)
-# 	def crawl(url, all_links):
-# 		if url in all_links:
-# 			return all_links
-# 		try:
-# 			import mechanicalsoup
-# 			browser = mechanicalsoup.StatefulBrowser()
-# 			browser.open(url)
-# 			scope = get_domain(url)
-# 			for link in browser.links():
-# 				_link = check_url(scope) + link.attrs['href']
-# 				if _link not in all_links and scope in _link:
-# 					all_links.append(_link)
-# 			for _url in all_links:
-# 				all_links = crawl(_url, all_links)
-# 		except Exception as error:
-# 			events.error(error)
-# 		finally:
-# 			return all_links
-# 	return crawl(url, all_urls)
-
+		return {url: params}
 
 all_urls = {}
 
@@ -51,7 +20,7 @@ def spider(url):
 	from modules import check_robots
 	global all_urls
 
-	# all_urls = check_robots.check(url)
+	# all_urls = check_robots.check(url) # TODO edit here
 	link = get_params(url)
 	link, params = link.keys()[0], link.values()[0]
 	all_urls.update({link: params})
@@ -79,7 +48,7 @@ def spider(url):
 					all_urls.update({link: params})
 				else: # Check and add params here
 					if params.keys()[0] not in all_urls[link].keys()[0]:
-						all_urls[link].update(params) # TODO test with multiple params
+						all_urls[link].update(params)
 
 	except Exception as error:
 		traceback.print_exc()
