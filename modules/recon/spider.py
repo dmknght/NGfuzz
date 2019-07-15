bcb1353c38a37d011092e35bca8c71031b1d8330
dmknght <dmknght@localhost.localdomain>
2019-07-15 10:47:11 +0700

    Update spider (yoyo motherfucker)

--- modules/recon/spider.py
+++ modules/recon/spider.py
@@ -20,30 +20,32 @@ def spider(url, branch = True):
 			# scope = cores.check_url("/".join(url.split("/")[2:-1]))
 			# scope = scope + "/" if scope[-1] != "/" else scope
 			scope = cores.check_url("/".join(url.split("/")[2:-1]))
-
+	visited = []
 	import mechanicalsoup
 	try:
 		browser = mechanicalsoup.StatefulBrowser()
-		browser.open(scope)
+		for spider_url in all_urls.keys():
+			if spider_url not in visited:
+				browser.open(spider_url)
+				visited.append(spider_url)
 
-		for link in browser.links():
-			link = cores.get_params(link.attrs['href'])
-			link, params = link.keys()[0], link.values()[0]
-			if link and "://" not in link:
-				if link[:2] == "./":
-					link = scope + link[2:]
-				elif link[0] == "/": # /index.php for example, remove / and combine with urls
-					link = scope[:-1] + link
-				else:
-					link = scope + link
-			if link and scope in link and "javascript:__" not in link and "javascript:" not in link:
-				if link not in all_urls.keys():
-					link = cores.check_url(link) # TODO bug here
-					# all_urls.append({link: params})
-					all_urls.update({link: params})
-				else: # Check and add params here
-					if params.keys()[0] not in all_urls[link].keys()[0]:
-						all_urls[link].update(params)
+				for link in browser.links():
+					link = cores.get_params(link.attrs['href'])
+					link, params = link.keys()[0], link.values()[0]
+					if link and "://" not in link:
+						if link[:2] == "./":
+							link = scope + link[2:]
+						elif link[0] == "/": # /index.php for example, remove / and combine with urls
+							link = scope[:-1] + link
+						else:
+							link = scope + link
+					if link and scope in link and "javascript:__" not in link and "javascript:" not in link:
+						if link not in all_urls.keys():
+							link = cores.check_url(link) # TODO bug here
+							all_urls.update({link: params})
+						else: # Check and add params here
+							if params.keys()[0] not in all_urls[link].keys()[0]:
+								all_urls[link].update(params)
 
 	except Exception as error:
 		from cores import events
