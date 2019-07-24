83d144d4ea522df97a49374c923824ec9d8940dc
dmknght <dmknght@protonmail.com>
2019-07-25 00:29:27 +0700

    Add break exception

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from cores import events
 import time
 
 runtime = time.time()
-url = cores.check_url("http://192.168.56.103/ghost/")
+url = cores.check_url("http://192.168.56.103/owaspbricks/")
 from modules.recon import footprinting
 footprinting.start(url)
 # if option spider
 from modules.recon import spider
 print("\n")
-branch = False
+branch = True
 events.success(url, "Spider")
 branches = spider.spider(url, branch = branch)
+print(branches)
 # if "no--crawl":
 # 	branches = [cores.get_params(url)]
 events.sub_info("Found %s URL[s]" %(len(branches)), info = "Root" if not branch else "Branch")
@@ -23,6 +24,10 @@ print("\n")
 events.info("Loaded %s modules: %s" %(len(modules), modules), info = "Scanner")
 from cores import scan
 for module in modules:
-	scan.get_method(branches, module)
+	try:
+		scan.get_method(branches, module)
+	except KeyboardInterrupt:
+		events.error("Interrupted by user", "SCAN")
+		break
 runtime = time.time() - runtime
 events.success(time.strftime("%Y-%m-%d %H:%M"), "Elapsed: %0.2f" %(runtime))
