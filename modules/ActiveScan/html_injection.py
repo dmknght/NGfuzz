5cad23ed295b2bf7ae09b57edaf75dc923117405
dmknght <dmknght@protonmail.com>
2019-07-27 17:14:33 +0700

    Remove html injection (vuln is similar xss)

--- modules/ActiveScan/html_injection.py
+++
@@ -1,8 +0,0 @@
-from cores.base_plugins import Scanner
-
-class Check(Scanner):
-	def gen_payload(self):
-		return ["<img src='x' onerror=alert(1)/>"]
-
-	def signature(self):
-		return {"HTML Injection" : self.payload}
