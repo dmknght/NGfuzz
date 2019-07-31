b16eb3d08a7b5eaf6cc5e7c5b61de46cad584280
dmknght <dmknght@protonmail.com>
2019-07-31 16:44:23 +0700

    Random xss payload

--- modules/ActiveScan/xss.py
+++ modules/ActiveScan/xss.py
@@ -2,7 +2,11 @@ from cores.base_plugins import Scanner
 
 class Check(Scanner):
 	def gen_payload(self):
-		return ["<script>alert(1);</script>"]
+		from cores.xeger import Xeger
+		generate = Xeger()
+		return [generate.xeger("((\%3C)|<)((\%2F)|\/)*[a-z0-9\%]+((\%3E)|>)")]
+
+		# return ["<script>alert(1);</script>"]
 
 	def signature(self):
 		return {"XSS" : self.payload}
