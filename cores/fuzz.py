8d58eb3745b4ffb43aea33c377cefe14d4187de3
dmknght <dmknght@protonmail.com>
2019-08-01 00:11:01 +0700

    Add template fuzz with inject point

--- cores/fuzz.py
+++ cores/fuzz.py
@@ -20,8 +20,50 @@ def get_method(url, params, fuzz_params, payloads, threads = 16):
 		# Wait for threads completed
 		for thread in threads:
 			thread.join()
+	
+	def send_inject(url, fuzz_param = "None", payload, inject_point = "*FUZZ*"):
+		# TODO parse fuzz_param
+		try:
+			import mechanicalsoup
+			# TODO add proxy
+			browser = mechanicalsoup.StatefulBrowser()
+			response = browser.open(url.replace(inject_point, payload)) # TODO url encode
+		except Exception as error:
+			from cores import events
+			events.error(error, "FUZZ")
+		finally:
+			browser.close()
+		
+		try:
+			resp = str(response.text)
+		except UnicodeEncodeError:
+			resp = str(response.text.encode('utf-8'))
+		except Exception:
+			resp = ""
+		
+		# TODO check HTTP code
+		import importlib, cores
+		from modules import ActiveScan
+		modules = cores.load_modules(ActiveScan)
+		for module in modules:
+			module = importlib.import_module('modules.%s' % (module))
+			module = module.Check()
 			
-	def send(url, params, fuzz_param, payload):
+			# Re-generate payload and signature. Work for XSS
+			module.payload = payload
+			module.signatures = module.signature()
+			
+			result = module.fuzz(url, payload, resp, fuzz_param)
+		# If not found any vuln, print something
+		if not result:
+			if response.status_code != 403:
+				from cores import events
+				events.fuzz_info(response.status_code, "GET", len(resp), fuzz_param, payload)
+		else:
+			from cores import events
+			events.fuzz_vuln(result, "GET", len(resp), fuzz_param, payload)
+	
+	def send_param(url, params, fuzz_param, payload):
 		try:
 			import mechanicalsoup
 			# TODO add proxy
@@ -75,7 +117,7 @@ def get_method(url, params, fuzz_params, payloads, threads = 16):
 					run_threads(workers)
 					del workers[:]
 				worker = threading.Thread(
-					target = send,
+					target = send_param,
 					args = (url, params, fuzz_param, payload)
 				)
 				worker.daemon = True
