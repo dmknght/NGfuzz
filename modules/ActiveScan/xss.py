38c184f73918a249f6bc4e395ca2e5385e1ef220
dmknght <dmknght@protonmail.com>
2019-07-31 23:27:35 +0700

    Add check payload for things seems like xss

--- modules/ActiveScan/xss.py
+++ modules/ActiveScan/xss.py
@@ -9,14 +9,13 @@ class Check(Scanner):
 			_payload = generate.xeger("((\%3C)|<)((\%69)|i|(\%49))((\%6D)|m|(\%4D))((\%67)|g|(\%47))[^\n]+((\%3E)|>)")
 			if any(x in _payload for x in "\"'><;/"):
 				return _payload
-
-	def check(self, url, payload, response, parameter):
+	
+	def fuzz(self, url, payload, response, parameter):
 		for injection_types in self.signatures.keys():
 			for sig in self.signatures[injection_types]:
 				match = re.findall(re.escape(sig), response)
 				if match and any(x in payload for x in "><"):
-					self.found(injection_types, url, parameter, payload)
-					return True
+					return self.signatures.keys()[0]
 		return False
 	
 	def signature(self):
