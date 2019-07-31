e96ac52aa34ed320b151d30bc321dbe494b883ef
dmknght <dmknght@protonmail.com>
2019-07-31 22:43:31 +0700

    Add check characters in payload (Non-safe payload)

--- modules/ActiveScan/xss.py
+++ modules/ActiveScan/xss.py
@@ -4,7 +4,10 @@ class Check(Scanner):
 	def gen_payload(self):
 		from cores.xeger import Xeger
 		generate = Xeger()
-		return [generate.xeger("((\%3C)|<)((\%69)|i|(\%49))((\%6D)|m|(\%4D))((\%67)|g|(\%47))[^\n]+((\%3E)|>)")]
+		while True:
+			_payload = generate.xeger("((\%3C)|<)((\%69)|i|(\%49))((\%6D)|m|(\%4D))((\%67)|g|(\%47))[^\n]+((\%3E)|>)")
+			if any(x in _payload for x in "\"'><;/"):
+				return _payload
 
 	def signature(self):
 		return {"XSS" : self.payload}
