7fdcd3506decf959b587fa2eaed4a4d6482d46b9
dmknght <dmknght@protonmail.com>
2019-08-18 02:25:18 +0700

    Add check vuln

--- cores/engines.py
+++ cores/engines.py
@@ -1,12 +1,15 @@
-from cores import makePayload
+import cores
+from cores import events
 
 
 def fuzz(url, params, values, payload, headers, point, method):
-	payload = makePayload(params, values, payload, point)
+	payload = cores.makePayload(params, values, payload, point)
 	response = send(url, method, payload, headers)
 	
-	
-	# TODO analysis response here
+	nameMethod = method.__name__.upper()
+	analysis(response, nameMethod, payload, point)
+	if response.status_code < 400:
+		checkVuln(payload, response.text, nameMethod, len(response.text), point)
 	# TODO fuzz headers
 	return True
 
@@ -14,3 +17,24 @@ def fuzz(url, params, values, payload, headers, point, method):
 def send(url, method, payload, headers):
 	resp = method(url, params = payload, headers = headers)
 	return resp
+
+
+def analysis(response, nameMethod, payload, point):
+	events.fuzz_info(response.status_code, nameMethod, len(response.text), point, payload)
+
+
+def checkVuln(payload, response, nameMethod, szResp, point):
+	import importlib
+	from modules import ActiveScan
+
+	modules = cores.load_modules(ActiveScan)
+
+	for module in modules:
+		module = importlib.import_module('modules.%s' % (module))
+		module = module.Check()
+		module.payload = payload
+		module.signatures = module.signature()
+		result = module.fuzz(payload, response, nameMethod, szResp, point)
+		if result:
+			from cores import events
+			events.fuzz_vuln(result, nameMethod, len(response), point, payload)
